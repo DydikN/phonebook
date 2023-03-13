5ae44eaab4cbf3362ee95ace617fa3a369380da4
DydikN <PingvinPain@gmail.com>
2023-03-14 00:21:14 +0200

    Update PublicRoute.js

--- src/components/PublicRoute/PublicRoute.js
+++ src/components/PublicRoute/PublicRoute.js
@@ -7,7 +7,7 @@ const PublicRoute = () => {
   const isLogin = useSelector(selectIsLoggedIn);
 
   if (isLogin) {
-    return <Navigate to="/contacts" />;
+    return <Navigate to="/" />;
   }
   return <Outlet />;
 };
