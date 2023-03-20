f382f65303348225a5d3766e0bec24d62aa59ff0
DydikN <PingvinPain@gmail.com>
2023-03-20 18:19:35 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,16 +3,10 @@ import { useAuth } from 'components/hooks/useAuth';
 import styles from './home-page.module.scss';
 
 const HomePage = () => {
-  const { isLoggedIn, user } = useAuth();
+  const { isLoggedIn } = useAuth();
 
   return (
     <>
-      {isLoggedIn && (
-        <h1 className={styles.title}>
-          {' '}
-          Hello,{user.name}, this is your private phonebook!
-        </h1>
-      )}
       {!isLoggedIn && <h1 className={styles.title}> Welcome to Phonebook!</h1>}
     </>
   );
